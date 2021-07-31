1421bc3feace0c2f999eb18c4105135292df7844
iliess-np <iliessnapolion@gmail.com>
2021-07-31 17:44:12 +0100

    added tracking / QR code / fetch data from the server

--- app/src/main/java/com/iliessnp/pfe/MainActivity.java
+++ app/src/main/java/com/iliessnp/pfe/MainActivity.java
@@ -74,8 +74,6 @@ public class MainActivity extends AppCompatActivity {
     LocationRequest locationRequest;
     LocationCallback locationCallback;
     FusedLocationProviderClient fusedLocationProviderClient;
-
-
     //Fetch data
     EditText editTextsenderid;
     Button buttonfetch;
